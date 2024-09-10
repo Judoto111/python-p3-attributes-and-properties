a9f726196a5c1c24e31f526dd012d317a31d1940
doreen chepkoech <doreenjudoto@gmail.com>
2024-09-10 15:53:49 +0300

    attributes.properties

--- lib/person.py
+++ lib/person.py
@@ -14,6 +14,31 @@ APPROVED_JOBS = [
     "Marketing",
     "Purchasing"
 ]
-
 class Person:
-    pass
+    def __init__(self, name, job):
+        self.name = name
+        self.job = job
+
+    # Getter and setter for 'name'
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+    
+    # Getter and setter for 'job'
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
